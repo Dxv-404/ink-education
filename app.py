61c5bb8f2d1823f616678559b59e57b14d5b1438
Dxv-404 <153078528+Dxv-404@users.noreply.github.com>
2025-08-04 13:02:23 +0530

    Fixed second critical security issue

--- app.py
+++ app.py
@@ -66,7 +66,8 @@ def check_session():
             print(f"Error restoring session: {str(e)}")
 
 # Firebase Admin Initialization
-cred = credentials.Certificate('SERVICE_ACCOUNT_KEY_PATH')
+cred_path= os.environ.get('SERVICE_ACCOUNT_KEY_PATH')
+cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
 
 # MongoDB Setup
